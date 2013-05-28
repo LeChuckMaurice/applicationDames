5ea9f060a25081df1fc8ab09d94b88874b2b15db
Clément Ha <duuche@gmail.com>
2013-05-28 10:25:51 +0200

    Création des méthodes de "Coup"

--- applicationDames/src/datas/Coup.java
+++ applicationDames/src/datas/Coup.java
@@ -2,4 +2,30 @@ package datas;
 
 public class Coup {
 
+	private int nbPrises;
+	private int nbPoints;
+	private Coordonnee[] parcours;
+
+	public Coup(){}
+
+	public int calculerPoints(){}
+
+	private boolean makeVulnerable(){}
+
+	public boolean isAuthorized(){}
+
+	public int getNbPoints(){}
+
+	public int getNbPrises(){}
+
+	public Coordonnee[] getParcours(){}
+
+	public Piece[] getPiecesPrises(){}
+
+	public void setNbPrises( int nbPrises ){}
+
+	public void setParcours( Coordonnee[] parcours){}
+
+
+
 }
