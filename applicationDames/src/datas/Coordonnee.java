ad7917f2c97e4d57b4babae4fe5085ceb11e0e68
GuillaumeP. <guillaume.perrudin@gmail.com>
2013-06-05 21:01:34 +0200

    Implémentation de la classe Coordonnee.java + Javadoc

--- applicationDames/src/datas/Coordonnee.java
+++ applicationDames/src/datas/Coordonnee.java
@@ -1,26 +1,60 @@
 package datas;
 
+/**
+ * La classe Coordonnee décrit une coordonnée en abscisse et en ordonnée.
+ */
 public class Coordonnee {
 
+	/**
+	 * Abscisse
+	 */
 	public int x;
+
+	/**
+	 * Ordonnée
+	 */
 	public int y;
 
-	public Coordonnee(){}
+	/**
+	 * Constructeur de la classe Coordonnee
+	 * @param x abscisse
+	 * @param y ordonnée
+	 */
+	public Coordonnee(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
 	
+	/**
+	 * Modificateur de l'attribut x
+	 * @param newX nouvelle abscisse
+	 */
 	public void setX(int newX){
-		x=newX;
+		this.x = newX;
 	}
 
+	/**
+	 * Modificateur de l'attribut y
+	 * @param newY nouvelle ordonnée
+	 */
 	public void setY(int newY){
-		y=newY;
+		this.y = newY;
 	}
 
+	/**
+	 * Accesseur de l'attribut x
+	 * @return x abscisse
+	 */
 	public int getX(){
-		return x;
+		return this.x;
 	}
 
+	/**
+	 * Accesseur de l'attribut y
+	 * @return y ordonnée
+	 */
 	public int getY(){
-		return y;
+		return this.y;
 	}
 
 }
