fb1a7dd6b234b92fdd28e17b5dea1f861bc32c24
GuillaumeP. <guillaume.perrudin@gmail.com>
2013-06-17 19:09:52 +0200

    Mise en commentaire de tests pour la compilation

---
+++ applicationDames/java/test/pda/TestCoup.java
@@ -0,0 +1,80 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import datas.*;
+
+public class TestCoup {
+
+	private Plateau plateau8;
+	private Plateau plateau10;
+	private Plateau plateau10Vide;
+	private Plateau plateau12;
+
+	private Piece[][] tabPieceVide10;
+
+	private Coup coupGenerique10;
+
+	@Before
+	public void setUp() throws Exception {
+
+		this.plateau8 = new Plateau(8);
+		this.plateau10 = new Plateau(10);
+		this.plateau12 = new Plateau(12);
+		this.plateau10Vide = new Plateau(10);
+
+		this.plateau8.remplirPlateau();
+		this.plateau10.remplirPlateau();
+		this.plateau12.remplirPlateau();
+
+		this.tabPieceVide10 = this.plateau10Vide.getTabPiece();
+
+		Piece piece = plateau10.getPiece(new Coordonnee(1,6));
+
+		this.coupGenerique10 = new Coup(piece, new Coordonnee(2,5));
+		
+	}
+
+	@Test
+	public void testFindPiecePrise(){
+		Dame dame1IA = new Dame(4,5,this.plateau10Vide,true);
+		this.tabPieceVide10[4][5] = dame1IA;
+
+		Pion pion1Joueur = new Pion(6,7,this.plateau10Vide,false);
+		this.tabPieceVide10[6][7] = pion1Joueur;
+
+		Coup coup1 = new Coup(dame1IA,new Coordonnee(8,9));
+		assertEquals(pion1Joueur, coup1.findPiecePrise());
+
+		Dame dame1Joueur = new Dame(6,3,this.plateau10Vide,false);
+		this.tabPieceVide10[6][3] = dame1Joueur;
+
+		Coup coup2 = new Coup(dame1IA,new Coordonnee(8,1));
+		assertEquals(dame1Joueur, coup2.findPiecePrise());
+
+		Coup coup3 = new Coup(dame1IA,new Coordonnee(2,3));
+		assertEquals(null, coup3.findPiecePrise());
+
+	}
+
+	@Test
+	public void testGetDepart() {
+		Coordonnee depart = this.coupGenerique10.getDepart();
+		assertEquals(depart.getX(),1);
+		assertEquals(depart.getY(),6);
+	}
+
+	@Test
+	public void testGetArrivee() {
+		Coordonnee arrivee = this.coupGenerique10.getArrivee();
+		assertEquals(arrivee.getX(),2);
+		assertEquals(arrivee.getY(),5);
+	}
+
+}
---
+++ applicationDames/java/test/pda/TestPiece.java
@@ -0,0 +1,81 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import datas.*;
+
+public class TestPiece {
+
+	private Plateau plateau10;
+
+	private Plateau plateauVide10;
+	private Piece[][] tabPieceVide10;
+
+	private Pion pionIA;
+	private Dame dameIA;
+
+	private Pion pionJoueur;
+	private Dame dameJoueur;
+
+	@Before
+	public void setUp() throws Exception {
+		this.plateau10 = new Plateau(10);
+
+		this.plateauVide10 = new Plateau(10);
+		
+		this.plateau10.remplirPlateau();
+
+		this.tabPieceVide10 = this.plateauVide10.getTabPiece();
+
+		this.pionIA = new Pion(4,5,this.plateauVide10,true);
+		this.tabPieceVide10[4][5] = this.pionIA;
+
+		this.dameIA = new Dame(1,0,this.plateauVide10,true);
+		this.tabPieceVide10[1][0] = this.dameIA;
+
+		this.pionJoueur = new Pion(6,7,this.plateauVide10,false);
+		this.tabPieceVide10[6][7] = this.pionJoueur;
+
+		this.dameJoueur = new Dame(2,9,this.plateauVide10,false);
+		this.tabPieceVide10[2][9] = this.dameJoueur;
+
+	}
+
+	@Test
+	public void testIsVulnerable() {
+		System.out.println(this.plateauVide10.toString());
+		assertFalse(this.pionIA.isVulnerable());
+
+		// pion peut prendre
+		this.plateauVide10.movePiece(pionJoueur,5,6);
+
+		System.out.println(this.plateauVide10.toString());
+		assertTrue(this.pionIA.isVulnerable());
+
+		// dameIA ne peut pas prendre car bloquée par pionIA
+		this.plateauVide10.movePiece(dameJoueur,0,7);
+		this.plateauVide10.movePiece(dameJoueur,3,4);
+
+		System.out.println(this.plateauVide10.toString());
+		assertFalse(this.pionIA.isVulnerable());
+		
+		this.plateauVide10.movePiece(dameJoueur,0,7);
+		this.plateauVide10.movePiece(pionJoueur,6,5);
+		
+		assertFalse(this.pionIA.isVulnerable());
+
+		this.plateauVide10.movePiece(dameJoueur,1,8);
+		
+		assertTrue(this.pionIA.isVulnerable());
+
+	}
+
+
+
+}
